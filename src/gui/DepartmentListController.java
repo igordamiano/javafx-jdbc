88763b96cad7976bdd7b0b385239cc7d905d2c98
Igor <igordamiano@gmail.com>
2020-03-27 18:11:26 -0300

    passando objeto departamento

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -48,7 +48,8 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		Department obj = new Department();
+		createDialogForm(obj, "/gui/DepartmentForm.fxml", parentStage);
 		
 	}
 	
@@ -81,12 +82,17 @@ public class DepartmentListController implements Initializable {
 		
 	}
 	
-	private void createDialogForm(String absoluteName, Stage parentStage) {
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		
 		try {
 			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			// Pegar o controller da tela que acabou de carregar (acima)
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj); // Carrega o objeto
+			controller.updateFormData(); // carrega os dados do objeto na tela
 			
 			// Dialog é um palco-stage na frente do outro
 			Stage dialogStage = new Stage();
